0090bf5a054846ff3ab58ad1b1efb3f62be7d5dd
stupidzgd <stupidzgd@163.com>
2018-07-21 23:09:54 +0800

    实现轮播图滑动

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
-
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="(item, index) in recommends" :key="index">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -15,8 +21,14 @@
 <script>
 import { getRecommend } from '@/api/recommend'
 import { ERR_OK } from '@/api/config'
+import Slider from 'base/slider/slider'
 
 export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
   created() {
     this._getRecommend()
   },
@@ -25,11 +37,14 @@ export default {
       this.$nextTick(() => {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>
