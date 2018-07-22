b20c2a7683ab896335c4ed1d7ebc9abbc3857d1c
stupidzgd <13426313932@163.com>
2018-07-23 00:05:05 +0800

    完成轮播图自动轮播以及索引功能，完成后台代理axios请求

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,7 +10,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
 const axios = require('axios')
-const bodyParser = require('body-parser')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
