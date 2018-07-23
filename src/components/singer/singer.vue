bc3a884e17b345556c3f870bb83fad4a914e806d
stupidzgd <stupidzgd@163.com>
2018-07-23 23:21:25 +0800

    获取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,8 +5,18 @@
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
 export default {
-
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        console.log(res.data)
+      })
+    }
+  }
 }
 </script>
 
